281049adf186f12f6bfd51127c2b3b145f1fdb54
Harshalk45 <harshalk1204@gmail.com>
2024-08-15 22:28:24 +0530

    update FormulaEvaluator.class

--- src/main/java/com/question1/FormulaEvaluator.java
+++ src/main/java/com/question1/FormulaEvaluator.java
@@ -1,11 +1,9 @@
 package com.question1;
 
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import org.mvel2.MVEL;
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -13,47 +11,68 @@ import java.util.Map;
 
 public class FormulaEvaluator {
 
-    public static void main(String[] args) throws IOException, ScriptException {
-        String jsonInput = "[{\"calculateField\":\"customFloat1\",\"formula\":\"10+15\"}," +
-                            "{\"calculateField\":\"customFloat2\",\"formula\":\"customFloat1*10\"}," +
-                            "{\"calculateField\":\"customFloat3\",\"formula\":\"customFloat2-18\"}," +
-                            "{\"calculateField\":\"customFloat4\",\"formula\":\"customFloat3+customFloat2\"}]";
+	public static void main(String[] args) {
+		String jsonInput = "[{\"calculateField\":\"customFloat1\",\"formula\":\"10+15\"},"
+				+ "{\"calculateField\":\"customFloat2\",\"formula\":\"customFloat1*10\"},"
+				+ "{\"calculateField\":\"customFloat3\",\"formula\":\"customFloat2-18\"},"
+				+ "{\"calculateField\":\"customFloat4\",\"formula\":\"customFloat3+customFloat2\"}]";
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<Calculation> calculations = objectMapper.readValue(jsonInput, new TypeReference<List<Calculation>>(){});
+		ObjectMapper objectMapper = new ObjectMapper();
+		List<Calculation> calculations;
 
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
-        Map<String, Double> results = new HashMap<>();
+		try {
+			calculations = objectMapper.readValue(jsonInput, new TypeReference<List<Calculation>>() {
+			});
+		} catch (IOException e) {
+			System.err.println("Failed to parse JSON input: " + e.getMessage());
+			return;
+		}
 
-        for (Calculation calc : calculations) {
-            String formula = calc.getFormula();
-            for (Map.Entry<String, Double> entry : results.entrySet()) {
-                formula = formula.replace(entry.getKey(), entry.getValue().toString());
-            }
+		Map<String, Object> context = new HashMap<>();
+		Map<String, Double> results = new HashMap<>();
 
-            Double result = (Double) engine.eval(formula);
-            results.put(calc.getCalculateField(), result);
-        }
+		for (Calculation calc : calculations) {
+			String formula = calc.getFormula();
 
-        results.forEach((key, value) -> System.out.println("FieldName: " + key + "\nValue: " + value));
-    }
+			for (Map.Entry<String, Double> entry : results.entrySet()) {
+				context.put(entry.getKey(), entry.getValue());
+			}
+
+			try {
+				Object evalResult = MVEL.eval(formula, context);
+				if (evalResult instanceof Number) {
+					results.put(calc.getCalculateField(), ((Number) evalResult).doubleValue());
+				} else {
+					System.err.println(
+							"The result of the formula is not a number for field '" + calc.getCalculateField() + "'");
+				}
+			} catch (Exception e) {
+				System.err.println("Failed to evaluate formula '" + formula + "' for field '" + calc.getCalculateField()
+						+ "': " + e.getMessage());
+			}
+		}
+
+		results.forEach((key, value) -> System.out.println("FieldName: " + key + "\nValue: " + value));
+	}
 }
 
 class Calculation {
-    public String getCalculateField() {
+	private String calculateField;
+	private String formula;
+
+	public String getCalculateField() {
 		return calculateField;
 	}
+
 	public void setCalculateField(String calculateField) {
 		this.calculateField = calculateField;
 	}
+
 	public String getFormula() {
 		return formula;
 	}
+
 	public void setFormula(String formula) {
 		this.formula = formula;
 	}
-	private String calculateField;
-    private String formula;
-
-    
 }
